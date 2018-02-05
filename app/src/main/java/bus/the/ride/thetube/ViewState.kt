2b1e9193abf8f6e6c2fdea68707fa8f7e327526c
David Shen <uashen@gmail.com>
2018-02-04 17:37:51 -0800

    oop generic

--- app/src/main/java/bus/the/ride/thetube/ViewState.kt
+++ app/src/main/java/bus/the/ride/thetube/ViewState.kt
@@ -1,7 +1,5 @@
 package bus.the.ride.thetube
 
-import bus.the.ride.thetube.api.NearbyStationsAndArrivals
-
 /**
  * Created by Shen on 1/31/2018.
  */
@@ -11,5 +9,5 @@ sealed class ViewState<T> {
     class Error<T>: ViewState<T>()
     class Empty<T>: ViewState<T>()
     class Loading<T>: ViewState<T>()
-    class DataReady<T>(val data : NearbyStationsAndArrivals): ViewState<T>()
+    class DataReady<T>(val data : T): ViewState<T>()
 }
